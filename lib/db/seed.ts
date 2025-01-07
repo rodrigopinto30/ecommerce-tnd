e9797a6f512968f5282d195682574f77640601c9
rodrigo <rodrigopinto5050@gmail.com>
2025-01-07 15:39:47 -0300

    04-Added-connect-to-mongodb-and-seed

---
+++ lib/db/seed.ts
@@ -0,0 +1,28 @@
+import { loadEnvConfig } from '@next/env'
+import { cwd } from 'process'
+import data from '../data'
+import { connectToDatabase } from '.'
+import Product from './models/product.model'
+
+loadEnvConfig(cwd())
+
+const main = async () => {
+  try {
+    const { products } = data
+    await connectToDatabase(process.env.MONGODB_URI)
+
+    await Product.deleteMany()
+    const createdProducts = await Product.insertMany(products)
+
+    console.log({
+      createdProducts,
+      message: 'Seeded database successfully',
+    })
+    process.exit(0)
+  } catch (error) {
+    console.log(error)
+    throw new Error('Failed to seed database')
+  }
+}
+
+main()
