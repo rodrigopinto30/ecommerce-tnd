a6d96890ace9f8b5b019416098adedf47dcc8341
rodrigo <rodrigopinto5050@gmail.com>
2025-02-18 19:05:26 -0300

    12-register-user

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,7 +1,13 @@
 'use server'
+
+import bcrypt from 'bcryptjs'
 import { signIn, signOut } from '@/auth'
-import { IUserSignIn } from '@/types'
+import { IUserSignIn, IUserSignUp } from '@/types'
 import { redirect } from 'next/navigation'
+import { UserSignUpSchema } from '../validator'
+import { connectToDatabase } from '../db'
+import { formatError } from '../utils'
+import User from '../db/models/user.model'
 
 export async function signInWithCredentials(user: IUserSignIn) {
   return await signIn('credentials', { ...user, redirect: false })
@@ -10,4 +16,25 @@ export async function signInWithCredentials(user: IUserSignIn) {
 export const SignOut = async () => {
   const redirectTo = await signOut({ redirect: false })
   redirect(redirectTo.redirect)
+}
+
+// Create
+export async function registerUser(userSignUp: IUserSignUp) {
+  try {
+    const user = await UserSignUpSchema.parseAsync({
+      name: userSignUp.name,
+      email: userSignUp.email,
+      password: userSignUp.password,
+      confirmPassword: userSignUp.confirmPassword,
+    })
+
+    await connectToDatabase()
+    await User.create({
+      ...user,
+      password: await bcrypt.hash(user.password, 5),
+    })
+    return { success: true, message: 'User created successfully' }
+  } catch (error) {
+    return { success: false, error: formatError(error) }
+  }
 }
